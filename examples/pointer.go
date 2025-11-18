1e97f6270b0b8b2c5bce3461c429734318153639
Mike Shock <mike.shock@ya.ru>
2025-11-18 18:03:36 +0500

    There were some changes made...

---
+++ examples/pointer.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type BaseType = string
+
+func main() {
+	var v BaseType = "Значение"  // переменная типа BaseType
+	var p *BaseType // указатель на переменную типа BaseType
+
+	p = &v  // ссылка на значение переменной типа BaseType
+	c := *p // значение переменной типа BaseType по ссылке на v
+
+	fmt.Println(v, p, *p, c)
+
+	changeValue(p)
+
+	fmt.Println(v, p, *p, c)
+}
+
+func changeValue(p *BaseType) {
+	var newValue BaseType = "Новое значение"
+	*p = newValue
+}
